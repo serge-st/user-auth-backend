5e436901216be03bd98f8e57f02d3b91c4245a75
Serge Stecenko <sergejs.stecenko@gmail.com>
2023-11-10 19:48:35 +0200

    build: add ConfigModule

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(
+    process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000,
+  );
 }
 bootstrap();
