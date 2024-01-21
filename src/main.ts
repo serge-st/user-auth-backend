ae0f07a9112eee838204ca422d3f99fa9365af7d
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-01-21 12:10:38 +0200

    feat: enable cors, allow access from fe app

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: 'http://localhost:3000' });
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
